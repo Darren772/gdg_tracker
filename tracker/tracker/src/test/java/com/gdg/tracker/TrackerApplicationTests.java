7fe7fd6d5734ebffbec5f8661ae0809aee64eeda
Darren772 <darrenmelvern27@gmail.com>
2026-07-07 14:42:47 +0700

    Initial Spring Boot skeleton from start.spring.io

---
+++ tracker/tracker/src/test/java/com/gdg/tracker/TrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gdg.tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
